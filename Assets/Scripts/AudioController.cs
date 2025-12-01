18e5cfe955546c3e29f012344c02328529a4d27c
zsukhera <zain2466sukhera@gmail.com>
2025-12-01 22:43:55 +0500

    Flyby

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -7,9 +7,28 @@ public class AudioController : MonoBehaviour
     public AudioSource audiosource;
     public AudioClip plasmaGun;
     public AudioClip flyby;
-    
+    public float flybyInterval = 10f; // interval in seconds
+    void Start()
+    {
+        StartCoroutine(PlayFlybyEvery10Seconds());
+    }
+
     public void playPlasmaGunSound()
     {
         audiosource.PlayOneShot(plasmaGun);
+    }
+
+    IEnumerator PlayFlybyEvery10Seconds()
+    {
+        while (true)
+        {
+            playFlybySound();
+            yield return new WaitForSeconds(flybyInterval); // wait 10 seconds
+        }
+    }
+
+    public void playFlybySound()
+    {
+        audiosource.PlayOneShot(flyby);
     }
 }
